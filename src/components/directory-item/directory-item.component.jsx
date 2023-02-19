5ffee0508e79d8e77a48e2804c2452e133042645
Mitesh Birru <79537705+mitibirru@users.noreply.github.com>
2023-02-19 19:14:45 +0530

    last tousches for styled components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
@@ -5,16 +6,15 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
+  const navigate = useNavigate();
   return (
-    // <Link to={`/shop/${category.title}`}>
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={() => navigate(category.route)}>
       <BackgroundImage imageUrl={category.imageUrl} />
       <Body>
         <h2>{category.title.toUpperCase()}</h2>
         <p>Shop Now</p>
       </Body>
     </DirectoryItemContainer>
-    // </Link>
   );
 };
 
